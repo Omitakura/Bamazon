eb8da1b4c5d3f6ac9304e27ec16621354aef3525
Zachary <Omitakura@gmail.com>
2020-02-02 23:50:01 -0600

    working customer.js and manager

---
+++ Customer.js
@@ -0,0 +1,65 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var conection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+});
+
+var displayProducts = function () {
+    var query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayTable = new Table({
+            head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    });
+}
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            name: "ID",
+            type: "input",
+            message: "Enter the Id of the item you would like to purchase.",
+            filter: Number
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many would you like to purchase?",
+            filter: Number
+        }
+    ]).then(function (answers) {
+        var quantityNeeded = answers.Quantity;
+        var IDrequested = answers.ID;
+        purchaseOrder(IDrequested, quantityNeeded);
+    });
+};
+function purchaseOrder(ID, amtNeeded) {
+    connection.query('SELECT * FROM products WHERE item_id = ' + ID, function (err, res) {
+        if (err) { console.log(err) };
+        if (amtNeeded <= res[0].stock_quantity) {
+            var totalCost = res[0].price * amtNeeded;
+            console.log("Enough units are in stock for you!");
+            console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + "is" + totalCost + "Thanks for coming!");
+        }
+    })
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+In this activity, I'll be creating an Amazon-like storefront with the MySQL skills I learned from KU Bootcamp. The app will take in orders from customers and deplete stock from the store's inventory. 
+
+I've also saved the required MySQL and inquirer packages for data input and storage.
